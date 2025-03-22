a462333f95b157e4d4e26d2736050d613333cb8d
piyumi <dlpnadeera1234@gmail.com>
2025-03-22 23:37:15 +0530

    Implementation

---
+++ Website/app.py
@@ -0,0 +1,63 @@
+import pickle
+import numpy as np
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+def prediction(lst):
+    filename = 'Model/lap_price_prediction.pickle'
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+    pred_value = model.predict([lst])
+    return pred_value
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    pred=0
+    if request.method == 'POST':
+        ram = request.form['ram']
+        weight = request.form['weight']
+        company = request.form['company']
+        typename = request.form['type']
+        opsys = request.form['os']
+        cpu = request.form['cpu']
+        gpu = request.form['gpu']
+        touchscreen = request.form.getlist('touchscreen')
+        ips = request.form.getlist('ips')
+
+        feature_list = []
+        feature_list.append(int(ram))
+        feature_list.append(float(weight))
+        feature_list.append(len(touchscreen))
+        feature_list.append(len(ips))
+
+        company_list = {'acer', 'apple','asus', 'dell', 'hp', 'lenovo','msi', 'other', 'toshiba'}
+        type_list = {'2in1convertible', 'gaming', 'netbook','notebook', 'ultrabook', 'workstation'}
+        os_list = {'linux', 'mac', 'other', 'windows'}
+        cpu_list = {'amd', 'intelcorei3', 'intelcorei5','intelcorei7', 'other',}
+        gpu_list = {'amd','intel', 'nvidia'}
+        
+
+        def traverse(lst,value):
+            for item in lst:
+                if value == item:
+                    feature_list.append(1)
+                else:
+                    feature_list.append(0)
+
+        traverse(company_list,company)
+        traverse(type_list,typename)
+        traverse(os_list,opsys)
+        traverse(cpu_list,cpu)
+        traverse(gpu_list,gpu)
+
+        pred = prediction(feature_list)*321.57
+        pred = np.round(pred[0],decimals=2)
+        
+    return render_template('index.html',pred = pred)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
